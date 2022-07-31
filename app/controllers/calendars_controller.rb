2bd78e2cce10267ecf742eb83ce8a1a54bc4354c
hasemasa1984 <hasemasa0719@gmail.com>
2022-07-31 10:37:25 +0900

    issue5 part2

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,5 @@
 class CalendarsController < ApplicationController
-  before_action : plan_params, if: :devise_controller?
+  before_action : plan_params, if: :devise_controller
 
 
   # １週間のカレンダーと予定が表示されるページ
@@ -17,7 +17,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan, :week)
   end
 
   def get_Week
@@ -25,7 +25,17 @@ class CalendarsController < ApplicationController
     
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
+    -<<<<<<< HEAD
+ 
+=======
+
+
+   
+>>>>>>> e5896c32f5dcd15d93da5507e4e061cba7eefa0c
+    
+<<<<<<< HEAD
+    
+>>>>>>> e5896c32f5dcd15d93da5507e4e061cba7eefa0c 
 
     @week_days = []
 
@@ -36,27 +46,16 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-<<<<<<< HEAD
  
       wday_num = Date.today.wday
       # wday_num = 今日の曜日の数値    ex) 今日が土曜日なら wday_num = 6
-=======
 
-
-      wday num = 7
->>>>>>> e5896c32f5dcd15d93da5507e4e061cba7eefa0c
      if 
        wday_num = wday_num -7
      end
       
-<<<<<<< HEAD
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wdays => wday_num }
-=======
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wdays => week_dates}
-=======
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
 
->>>>>>> e5896c32f5dcd15d93da5507e4e061cba7eefa0c
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wdays => wday_num }
       @week_days.push(days)
     end
   end
